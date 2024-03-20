0de2e9c0e0c769edd1c23fd176914a4f847033e4
Eduardo Oliveira <edurafael.deoliveira@gmail.com>
2024-03-20 11:22:47 -0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let nome = "eduardo";
+let idade = 91;
+console.log(`Meu nome é ${nome} e minha idade é ${idade}`);
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+let nome: string = "eduardo";
+let idade: number = 91;
+console.log(`Meu nome é ${nome} e minha idade é ${idade}`);
